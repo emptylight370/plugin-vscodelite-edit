11d355da187fb2197ba0be0ed97b813582538b35
Emptylight <1378990254@qq.com>
2026-02-02 20:29:32 +0800

    refactor: 换用更合适的方法引入主题定义

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import {} from 'siyuan';
 import { _getFile } from './api';
-import { ThemeConfig } from './types/theme.d';
+import { ThemeConfig } from 'siyuan-vscodelite-edit/src/ts/types.d';
 
 export async function getThemeLastSeenVersion() {
     const config: ThemeConfig = JSON.parse(await _getFile('/data/snippets/vsc_edit.config.json'));
