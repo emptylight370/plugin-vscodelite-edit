9f419ece3f536e3ac04128d5953122895038cfa0
Emptylight <1378990254@qq.com>
2026-02-03 16:40:29 +0800

    refactor: 换用靠谱的方法引入依赖，简化代码
    并且更新主题版本，适应最新修改

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,7 +1,24 @@
 import { Menu, Protyle } from 'siyuan';
+import { ThemeConfig, vscCounters, vscMessage, vscObservers, vscTimers } from 'siyuan-vscodelite-edit/src/ts/types';
 
 interface IClickBlockIconEventDetail {
     menu: Menu;
     protyle: Protyle;
     blockElements: HTMLElement[];
+}
+
+declare global {
+    // 主题代码中添加的全局变量
+    /** 默认配置文件 */
+    var vscDefaultConf: Readonly<ThemeConfig>;
+    /** 本地化提示信息 */
+    var vscMessage: Readonly<vscMessage>;
+    /** 默认语言，可由浏览器方法获取 */
+    var vscLang: keyof vscMessage['language'];
+    /** 目前所有的计时器 */
+    var vscTimers: vscTimers;
+    /** 目前所有的观察器 */
+    var vscObservers: vscObservers;
+    /** 目前所有的计数器 */
+    var vscCounters: vscCounters;
 }
